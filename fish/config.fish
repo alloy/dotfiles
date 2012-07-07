60a4a7166272701624bbc7965590d256e65f731c
Eloy Duran <eloy.de.enige@gmail.com>
2012-07-07 18:50:24 +0200

    Import intial config.fish

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+### fish
+
+set fish_greeting ""
+
+# source all of the files in the function directory
+for f in (find ~/.config/fish/functions -type f)
+  . $f
+end
+
+
+### general
+
+set PATH /opt/local/bin $PATH
+
+# vim and some other things rely on SHELL being a posixy sh this may cause
+# problems in the future, in which case I'll define a little function to wrap
+# vim and give it a fake environment variable.
+set SHELL (which sh)
+
+function -d "Open the working directory with mvim" m
+  mvim .
+end
+
+
+### rbenv
+
+set PATH $HOME/.rbenv/bin $PATH
+set PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
